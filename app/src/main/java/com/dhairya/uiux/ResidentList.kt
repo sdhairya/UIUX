45e854c4caeb859efe0fa15b6be48ed94c5bc9e4
Hardik <hardikvaghasiya69@gmail.com>
2022-02-12 01:05:34 +0530

    abc

--- app/src/main/java/com/dhairya/uiux/ResidentList.kt
+++ app/src/main/java/com/dhairya/uiux/ResidentList.kt
@@ -1,7 +1,9 @@
 package com.dhairya.uiux
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ListView
 
 class ResidentList : AppCompatActivity() {
@@ -9,6 +11,14 @@ class ResidentList : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.resident_list)
 
-        val resident_list = findViewById<ListView>(R.id.resident_list)
+        var resident_list = findViewById<ListView>(R.id.resident_list)
+        var showdialogdemo = findViewById<Button>(R.id.showdialogdemo)
+
+        showdialogdemo.setOnClickListener {
+
+            val dialog = Dialog(this)
+            dialog.setContentView(R.layout.resident_details_dialog)
+            dialog.show()
+        }
     }
 }
