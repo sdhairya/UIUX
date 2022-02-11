d866df561322daf80c533330be836988f10071f6
sdhairya <sdhairya412@gmail.com>
2022-02-11 23:46:17 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/com/dhairya/uiux/ResidentList.kt
+++ app/src/main/java/com/dhairya/uiux/ResidentList.kt
@@ -2,13 +2,17 @@ package com.dhairya.uiux
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.ListView
 
 class ResidentList : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.resident_list)
 
-        val resident_list = findViewById<ListView>(R.id.resident_list)
+        var resident_list = findViewById<ListView>(R.id.resident_list)
+
+
     }
 }
